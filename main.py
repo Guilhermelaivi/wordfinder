601e1576714ec9f181542572f0a17636fe6d01f7
guilhermelaivi <guilhermelaivi@gmail.com>
2023-06-21 15:25:43 -0300

    The logic of comparison was created

--- main.py
+++ main.py
@@ -6,5 +6,16 @@ listaComentarios = comentarios["comentarios"].tolist()
 palavrasChaves = pd.read_excel("./baseDeDados.xlsx")
 listaPalavras = palavrasChaves ["SUDIR"].tolist()
 
+comentariosFinais = []
+
+for comentario in listaComentarios:  
+    for palavra in listaPalavras: 
+        if str(palavra) in str(comentario):
+            comentariosFinais.append(comentario) 
+            
+            break
+
+print(comentariosFinais)
+                
 
  

654655e53f77a7e8510cc1000e4241e736b6d063
guilhermelaivi <guilhermelaivi@gmail.com>
2023-06-21 14:51:31 -0300

    Open and reader second table 'basededados'

--- main.py
+++ main.py
@@ -3,4 +3,8 @@ import pandas as pd
 comentarios = pd.read_excel("./comentarios.xlsx")
 listaComentarios = comentarios["comentarios"].tolist()
 
+palavrasChaves = pd.read_excel("./baseDeDados.xlsx")
+listaPalavras = palavrasChaves ["SUDIR"].tolist()
 
+
+ 
